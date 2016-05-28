6a5bef0694638004dcb635edc0833d1e78ca467c
J LEE <ririellelee@gmail.com>
2016-05-29 00:44:06 +1200

    map, googlmap not yet

--- app/scripts/pages/map.jsx
+++ app/scripts/pages/map.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Map extends React.Component {
 
     render() {
         return (
             <div className="map-container">
-                MAP
+                <ul className="map-tab">
+                	<li><Link to="/">Bus list</Link></li>
+                	<li className="tab-active"><Link to="/">Map</Link></li>
+                </ul>
+                <div id="googlemap"> d               
+                </div>
+                
             </div>
         );
     }
