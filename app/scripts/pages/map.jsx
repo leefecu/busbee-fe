bd9c9320e436353d3d729830f611c2560b6b30f7
Ken Lee <leefecu@gmail.com>
2016-05-29 10:02:56 +1200

    resolve conflict

--- app/scripts/pages/map.jsx
+++ app/scripts/pages/map.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 class Map extends React.Component {
 
     render() {
         return (
             <div className="map-container">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                MAPs
-=======
-                MAP
->>>>>>> master
-=======
                 <ul className="map-tab">
                 	<li><Link to="/">Bus list</Link></li>
                 	<li className="tab-active"><Link to="/">Map</Link></li>
@@ -24,7 +13,6 @@ class Map extends React.Component {
                 <div id="googlemap"> d               
                 </div>
                 
->>>>>>> 8399f35a6b0f0db98859908a90aa16cd1d293475
             </div>
         );
     }
