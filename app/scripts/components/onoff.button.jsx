223f152074710d4b923d9a74f2cc5b2ba83ea0f4
[Brian Kim] <keepboy50@hotmail.com>
2016-05-30 11:46:50 +1200

    feature/alarm-list first layout

---
+++ app/scripts/components/onoff.button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class onOffButton extends React.Component {
+
+
+
+    render() {
+        return (
+
+            // <div className="onoffswitch">
+            //     <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" checked={ this.props.alarmType === "Y" ? "true" : ""}  />
+            //     <label className="onoffswitch-label" htmlFor="myonoffswitch">
+            //         <span className="onoffswitch-inner"></span>
+            //         <span className="onoffswitch-switch"></span>
+            //     </label>
+            // </div>
+
+            <div className="onoffswitch">
+            <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" defaultChecked={ this.props.alarmType === "Y" ? "true" : ""}  />
+            <label className="onoffswitch-label" htmlFor="myonoffswitch">
+                <span className="onoffswitch-inner"></span>
+                <span className="onoffswitch-switch"></span>
+            </label>
+            </div>
+        )
+    }
+};
+
+export default onOffButton;
--- app/scripts/pages/alarm.jsx
+++ app/scripts/pages/alarm.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import OnOffButton from '../components/onoff.button.jsx'
+import 'whatwg-fetch'
 
 
 class Alarm extends React.Component {
 
+
+
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            data: []
+        }
+    }
+
+    componentWillMount () {
+        const self = this
+        fetch('http://localhost:3000/alarmList')
+            .then(function(response) {
+                return response.text()
+            }).then(function(body) {
+                var lists = JSON.parse(body)
+                self.setState({data: lists})
+            }).catch(function(ex) {
+                console.log('Error', ex)
+            })
+    }
+
+    componentDidMount () {}
+
+    componentWillReceivnpeProps () {}
+
+    componentWillUpdate () {}
+
+    componentDidUpdate () {}
+
+    componentWillUnmount () {}
+
+    renderResult(row, index) {
+        return (
+            <tr key={index}>
+                <td>{ row.num }</td>
+                <td>{ row.stop }</td>
+                <td><OnOffButton alarmId={row.id} alarmType={row.type} /></td>
+            </tr>
+        )
+    }
+
     render() {
         return (
-        	<div className="alarm-list">
+            <div className="alarm-list">
                 <table>
                     <thead>
                         <tr>
@@ -15,47 +61,67 @@ class Alarm extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr><a href="#">
-                            <td>887</td>
-                            <td>2475 - Constellation</td>
-                            <td><img src="../images/content/stop_black.png" /></td>
-                        </a></tr>
-                        <tr>
-                            <td>887</td>
-                            <td>2475 - Constellation</td>
-                            <td><img src="../images/content/bus_black.png" /></td>
-                        </tr>
-                        <tr>
-                            <td>656</td>
-                            <td>4334 - Hibury Shops</td>
-                            <td><img src="../images/content/stop_black.png" /></td>
-                        </tr>
-                        <tr>
-                            <td>887</td>
-                            <td>2475 - Constellation</td>
-                            <td><img src="../images/content/bus_black.png" /></td>
-                        </tr>
-                        <tr>
-                            <td>656</td>
-                            <td>4334 - Hibury Shops</td>
-                            <td><img src="../images/content/bus_black.png" /></td>
-                        </tr>
-                        <tr>
-                            <td>887</td>
-                            <td>2475 - Constellation</td>
-                            <td><img src="../images/content/bus_black.png" /></td>
-                        </tr>
-                        <tr>
-                            <td>656</td>
-                            <td>2475 - Constellation</td>
-                            <td><img src="../images/content/bus_black.png" /></td>
-                        </tr>
-
+                        {
+                            this.state.data.map(this.renderResult)
+                        }
                     </tbody>
                 </table>
             </div>
         );
     }
+    // render() {
+    //     return (
+    //      <div className="alarm-list">
+    //             <table>
+    //                 <thead>
+    //                     <tr>
+    //                         <th>Bus</th>
+    //                         <th>Bus Stop</th>
+    //                         <th>On/Off</th>
+    //                     </tr>
+    //                 </thead>
+    //                 <tbody>
+    //                     <tr><a href="#">
+    //                         <td>887</td>
+    //                         <td>2475 - Constellation</td>
+    //                         <td><img src="../images/content/stop_black.png" /></td>
+    //                     </a></tr>
+    //                     <tr>
+    //                         <td>887</td>
+    //                         <td>2475 - Constellation</td>
+    //                         <td><img src="../images/content/bus_black.png" /></td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>656</td>
+    //                         <td>4334 - Hibury Shops</td>
+    //                         <td><img src="../images/content/stop_black.png" /></td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>887</td>
+    //                         <td>2475 - Constellation</td>
+    //                         <td><img src="../images/content/bus_black.png" /></td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>656</td>
+    //                         <td>4334 - Hibury Shops</td>
+    //                         <td><img src="../images/content/bus_black.png" /></td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>887</td>
+    //                         <td>2475 - Constellation</td>
+    //                         <td><img src="../images/content/bus_black.png" /></td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>656</td>
+    //                         <td>2475 - Constellation</td>
+    //                         <td><img src="../images/content/bus_black.png" /></td>
+    //                     </tr>
+
+    //                 </tbody>
+    //             </table>
+    //         </div>
+    //     );
+    // }
     
 };
     
