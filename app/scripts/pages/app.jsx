3dadfa75b2997ebcaf9d173a9428ca8f5fb6ffbb
[Brian Kim] <keepboy50@hotmail.com>
2016-06-30 21:18:03 +1200

    resolve conflict and re-register alarm-result with phone vibration

--- app/scripts/pages/app.jsx
+++ app/scripts/pages/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
+
 
 class App extends React.Component {
     constructor(props){
@@ -19,39 +21,56 @@ class App extends React.Component {
 
     }
 
-    componentDidMount() {
+    class App extends React.Component {    
+        componentDidMount(){
+            //set resize function : when the browser is resized.
+            window.addEventListener('resize', this.handleResize.bind(this));  
+            this.handleResize();
 
-    if (this.state.userId === "user01") {
-        const self = this
-            setInterval(function() {
-                fetch('http://localhost:3000/alarmResult')
-                .then(function(response) {
-                    return response.text()
-                }).then(function(body) {
-                   var lists = JSON.parse(body)
-                   console.log(lists[0])
-                   console.log(navigator.vibrate(3000)) // Phone vibration supported by HTML5)
-                }).catch(function(ex) {
-                    console.log('Error', ex)
-                })
-             }, 3000); 
-            console.log("The user " + "[" + this.state.userId + "]" + " has been successfully logged in")
-    } else {
-        console.log("You are not Logged in")
+        if (this.state.userId === "user01") {
+            const self = this
+                setInterval(function() {
+                    fetch('http://localhost:3000/alarmResult')
+                    .then(function(response) {
+                        return response.text()
+                    }).then(function(body) {
+                       var lists = JSON.parse(body)
+                       console.log(lists[0])
+                       console.log(navigator.vibrate(3000)) // Phone vibration supported by HTML5)
+                    }).catch(function(ex) {
+                        console.log('Error', ex)
+                    })
+                 }, 3000); 
+                console.log("The user " + "[" + this.state.userId + "]" + " has been successfully logged in")
+        } else {
+            console.log("You are not Logged in")
+        }
+    }  
+
+    handleResize(){
+        //get window height size
+        var windowH = window.innerHeight;
+        //get footer height size
+        var footerH = document.getElementById('ft').clientHeight;
+        //get header height size : header size is 0px in Main(home) page
+        var headerH = 0;
+        if(this.props.location.pathname != '/'){
+            //get header height size
+            headerH = ReactDOM.findDOMNode(this).children[0].clientHeight;
+        }
+        //get content div height size : whole window height size - header and footer height size
+        var contentH = windowH - (headerH+footerH);
+        //set content height size
+        document.getElementById("cont").style.height = contentH+"px"; 
     }
-
-
-}
-   
-
 
 
     render() {
         return (
             <div className="container">
-                { this.props.location.pathname === '/' ? null : <Header pathname={this.props.location.pathname} />}
+                { this.props.location.pathname === '/' ? null : <Header pathname={this.props.location.pathname} pathRouteName={this.props.routes[1].name}/>}
 
-                <div className="content">
+                <div className="content" id="cont">
                     {this.props.children}
                 </div>
 
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -5,6 +5,7 @@ import createHistory from 'history/lib/createHashHistory';
 import App from './pages/app.jsx';
 import Home from './pages/home.jsx';
 import SearchResult from './pages/search.result.jsx';
+import StopResult from './pages/stop.result.jsx';
 import Map from './pages/map.jsx';
 import Favorite from './pages/favorite.jsx';
 import NotFound from './pages/notFound.jsx';
@@ -18,12 +19,13 @@ const routes = (
   <Router history={createHistory(historyOptions)}>
     <Route path='/' component={ App }>
       <IndexRoute component={ Home }/>
-      <Route path='home' component={ Home } />
-      <Route path="search/:saerchVal" component={SearchResult}/>
-      <Route path='alarm' component= { Alarm }/>
-      <Route path='map' component={ Map } />
-      <Route path='favorite' component={ Favorite } />
-      <Route path='*' component={NotFound}/>
+      <Route name="dashboard" path='home' component={ Home } />
+      <Route name="dashboard-child" path="search/:saerchVal" component={SearchResult}/>
+      <Route name="dashboard" path='alarm' component= { Alarm }/>
+      <Route name="dashboard" path='map' component={ Map } />
+      <Route name="dashboard" path='stop.result' component={ StopResult } />
+      <Route name="dashboard" path='favorite' component={ Favorite } />
+      <Route name="dashboard" path='*' component={NotFound}/>
     </Route>
   </Router>
 );
